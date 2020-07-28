a22284dfec3b736df0ef182c3aa16edc8ae16a6e
caleb-archer <55740865+caleb-archer@users.noreply.github.com>
2020-07-29 09:36:06 +1200

    update

---
+++ intro_input_testing_v2.py
@@ -0,0 +1,26 @@
+#create an introduction to the game. and ask what car number the user would like to choose.
+
+
+print("kia ora\nwelcome to my car game! :) peasant")
+print("_______________________")
+#level = input('which level would you like to play, easy or difficult?')
+def carcheck (question, min, max):
+    #def = define
+    # intcheck is the name of my fucntion in python
+    # question - will be the question asked to the user
+    # low and high boundaries will be set up by the progarmmer
+    valid = False
+    while not valid:
+        try: #try to convet input to interger
+            initial_car = int(input(question))
+            if min<= initial_car<=max:
+                print('Yay you chose car', initial_car)
+                # I am happy with the loop
+                break
+            else: #the number is out of bounds
+                print("oi!\npeasant, do what you are told and enter a number between 1 and 12")
+                continue
+        except ValueError:
+            print("oi!\npeasant, do what you are told and enter a valid car number\nbetween 1 and 12")
+#run function - call
+carcheck("Choose a car number... (1-12)? ", 1 , 12)
