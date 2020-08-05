4c52d4e20fed82b5ac604947adadfdd3080e1ad7
caleb-archer <55740865+caleb-archer@users.noreply.github.com>
2020-08-05 15:30:10 +1200

    update

---
+++ sleep_fucntion test.py
@@ -0,0 +1,19 @@
+import time
+import sys
+
+print("hello, how are you today")
+
+for i in range(8):
+    sys.stdout.write(".")
+    time.sleep(0.3)
+
+print("\nthats very good to hear")
+
+print("hello")
+
+#x = "....."
+#for i in range(10):
+    #print(x)
+   # time.sleep(4)
+    #x += x
+
